6169a7eff02edb6dde126d900d4b24a2caf37937
isitavi <avijitdasnoyon@gmail.com>
2019-03-03 14:52:17 +0600

    Grab all the problem links

--- category.py
+++ category.py
@@ -4,20 +4,40 @@ from fake_useragent import UserAgent
 
 
 ua = UserAgent()
-url = "https://codingbat.com/java"
+baseURL = "https://codingbat.com"
+requestURL = "https://codingbat.com/java"
 headers = {'user-agent': ua.chrome}
 
 
 # Response
-response = requests.get(url, headers=headers)
+response = requests.get(requestURL, headers=headers)
 # print(response.content)
 
 soup = BeautifulSoup(response.content, 'lxml')
 expecteDivs = soup.findAll("div", {"class": "summ"})
 
-allResource = {
-    allData.a.span.text: allData.a['href'] for allData in expecteDivs}
-print(allResource)
+# Dict Comprehension
+# allResource = {
+#     allData.a.span.text: baseURL+allData.a['href'] for allData in expecteDivs}
+# print(allResource)
 
+#
 # for alldata in expecteDivs:
 #     print(alldata)
+
+
+# List Comprehension
+allResource = [baseURL+allData.a['href'] for allData in expecteDivs]
+
+for innerlink in allResource:
+    individualLink = requests.get(innerlink, headers=headers)
+    innerSoup = BeautifulSoup(individualLink.content, 'lxml')
+    targetDivs = innerSoup.find("div", {"class": "tabin"})
+    questionURL = [baseURL+td.a['href']
+                   for td in targetDivs.table.find_all('td')]
+    for url in questionURL:
+        print(url)
+    break
+
+    # allCategory = [allCat.table.tr.td.a['href'] for allCat in targetDivs]
+    # print(allCategory)
